5b9d0697d5cc1dc9d00b5d8c3b6706316cc9ac0c
Dan-cmyk <dante.grenholm.9198@student.uu.se>
2023-05-04 12:53:55 +0200

    Beginning of a websocket implementation, does not work

--- EV3/connect.py
+++ EV3/connect.py
@@ -2,17 +2,20 @@
 import socket
 import asyncio
 
+# Port and ip to server
+PORT = 8081
+IP = "192.168.1.56"
+
+# Needed for the pybricks version of asyncio
+# Gives us a usable address to connect to
+socket_address = socket.getaddrinfo(IP, PORT)[0][-1]
+
 
 def sse_mvp():
     """
         MVP for SSE connection
         This code is not async so will only work until the robot has to do more then listen to commands
     """
-
-    # Needed for the pybricks version of asyncio
-    # Gives us a usable address to connect to
-    socket_address = socket.getaddrinfo("192.168.1.56", 8081)[0][-1]
-
     # Create a socket client
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -23,11 +26,10 @@ def sse_mvp():
 
     print("Socket connected\n")
 
-    handshake = b"GET / HTTP/1.1\r\n\
-                   Cache-Control: no-cache\r\n\
-                   Connection: keep-alive\r\n\
-                   Accept: */*\r\n\
-                   Host: 192.168.1.56:8081\r\n\r\n"
+    handshake = "GET / HTTP/1.1\r\n\
+                Cache-Control: no-cache\r\n\
+                Connection: keep-alive\r\n\
+                Host: {0}:{1}\r\n\r\n".format(IP, PORT).encode()
     # Tell the server that you are connected and listening
     client.send(handshake)
 
@@ -41,5 +43,37 @@ def sse_mvp():
         print(response)
 
 
+def websocket_mvp():
+    # Create a socket client
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    print("Socket created")
+
+    # Connect to remote socket server
+    client.connect(socket_address)
+
+    print("Socket connected\n")
+
+    handshake = "GET / HTTP/1.1\r\n\
+    Host: {0}:{1}\r\n\
+    Origin: http://example.com\r\n\
+    Connection: Upgrade\r\n\
+    Upgrade: websocket\r\n\
+    Sec-WebSocket-Key: q4xkcO32u266gldTuKaSOw==\r\n\
+    Sec-WebSocket-Version: 13\r\n\r\n".format(IP, PORT).encode()
+    print(handshake.decode())
+    # Tell the server that you are connected and listening
+    client.send(handshake)
+
+    # Print server response, should check what the response code is
+    # If the response code is not 200 something is wrong
+    print(client.recv(1024).decode())
+
+    # while True:
+    #     response = client.recv(1024)
+    #     print(response)
+
+
 if __name__ == "__main__":
-    sse_mvp()
+    # sse_mvp()
+    websocket_mvp()
